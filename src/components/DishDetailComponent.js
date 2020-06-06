d02885265fb3e3d279c11109a2e9cfcd8f9076dc
Shubham <shubhamsingh397@gmail.com>
2020-06-07 00:31:43 +0530

    React Animation Components

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
-
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 
 
@@ -35,13 +35,19 @@ import { baseUrl } from '../shared/baseUrl';
              return(
                
                      <div  className="col-12 col-md-5 m-1">
-                 <Card>
-                     <CardImg top src={baseUrl + dish.image} alt={dish.name} />
-                     <CardBody>
-                     <CardTitle>{dish.name}</CardTitle>
-                     <CardText>{dish.description}</CardText>
-                     </CardBody>
-                 </Card>
+               <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
+            <Card>
+                <CardImg top src={baseUrl + dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+            </FadeTransform>
                  </div>
                 
              );
@@ -56,8 +62,11 @@ import { baseUrl } from '../shared/baseUrl';
             if (comments == null) {
                 return (<div></div>)
             }
+            
                 const showComments = comments.map(comment=> {
                     return (
+                        
+                        <Fade in>
                         <li key={comment.id} >
                             <p>{comment.comment}</p>
                             <p>--{comment.author},
@@ -69,14 +78,19 @@ import { baseUrl } from '../shared/baseUrl';
                         }).format(new Date(comment.date))}
                             </p>
                         </li>
-                    )
+                        </Fade>
+                       
+                    );
                 })
+                 
 
                 return (
                     <div className='col-12 col-md-5 m-1'>
                         <h4> Comments </h4>
                         <ul className='list-unstyled'>
+                        <Stagger in>
                             {showComments}
+                            </Stagger>
                         </ul>
                         <CommentForm dishId={dishId} postComment={postComment} />
                     </div>
