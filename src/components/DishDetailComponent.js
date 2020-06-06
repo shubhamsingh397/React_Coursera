a28d4b4df2735c758bbfc38209e69e892085c17a
Shubham <shubhamsingh397@gmail.com>
2020-06-06 23:12:02 +0530

    Fetch from Server

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -4,7 +4,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbIte
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
-
+import { baseUrl } from '../shared/baseUrl';
 
 
 
@@ -36,7 +36,7 @@ import { Loading } from './LoadingComponent';
                
                      <div  className="col-12 col-md-5 m-1">
                  <Card>
-                     <CardImg top src={dish.image} alt={dish.name} />
+                     <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                      <CardBody>
                      <CardTitle>{dish.name}</CardTitle>
                      <CardText>{dish.description}</CardText>
